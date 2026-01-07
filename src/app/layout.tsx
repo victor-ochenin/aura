5e17a63dfe6d2a3fd739456afa4701ebb7c2e23e
victor-ochenin <ovsynikovvitaly@gmail.com>
2026-01-07 16:23:53 +0300

    04: фоновая работа

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { IBM_Plex_Mono, Inter } from "next/font/google";
 
 import { Providers } from "@/components/providers";
-import { ThemeProvider } from "@/components/theme-provider";
 
 import "./globals.css";
 
