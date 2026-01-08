1636cd10d88471b985c301e55db4530d0cbd9136
victor-ochenin <ovsynikovvitaly@gmail.com>
2026-01-08 10:00:53 +0300

    05: fireceawl ai

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,16 +1,49 @@
 import { generateText } from "ai";
 import { inngest } from "./client";
 import { google } from "@ai-sdk/google";
+import { firecrawl } from "@/lib/firecrawl";
+
+const URL_REGEX = /https?:\/\/[^\s]+/g;
 
 export const demoGenerate = inngest.createFunction(
   { id: "demo-generate" },
   { event: "demo/generate" },
-  async ({ step }) => {
-    await step.run("generate-text", async () => {
-        return await generateText({
-            model: google('gemini-2.5-flash'),
-            prompt: 'Write a vegetarian lasagna recipe for 4 people.',
-        });
+  async ({ event, step }) => {
+    const { prompt } = event.data as { prompt: string; };
+
+    const urls = await step.run("exctract-urls", async () => {
+      return prompt.match(URL_REGEX) ?? [];
+    }) as string[];
+
+    const scrapedContent = await step.run("scrape-urls", async () => {
+      const results = await Promise.all(
+        urls.map(async (url) => {
+          const result = await firecrawl.scrape(
+            url,
+            { formats: ["markdown"] },
+          );
+          return result.markdown ?? null;
+        })
+      );
+      return results.filter(Boolean).join("\n\n");
     });
-  },
+
+    const finalPrompt = scrapedContent
+      ? `Context:\n${scrapedContent}\n\nQuestion: ${prompt}`
+      : prompt;
+
+    const generated = await step.run("generate-text", async () => {
+      return await generateText({
+        model: google('gemini-2.5-flash'),
+        prompt: finalPrompt,
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
+      });
+    });
+
+    return generated;
+  }
 );
